9e10ddb642d6961eeb2ecdd0e370363eea498daa
Murtaza <murtazamandasore@gmail.com>
2025-07-30 18:23:13 +0500

    question 8 done

--- question7.dart
+++ question7.dart
@@ -9,12 +9,7 @@ void main() {
 Check if "fri" exist in expanses; if exist change it's value to 5000.0 otherwise add 'fri' to expenses 
 and set its value to 5000.0 then print expenses.
    */
-  Map<String, double> expenses = {
-    'sun': 3000.0,
-    'mon': 3000.0,
-    'tue': 3234.0,
-    'fri': 2133.2,
-  };
+  Map<String, double> expenses = {'sun': 3000.0, 'mon': 3000.0, 'tue': 3234.0};
   expenses.update('fri', (value) => value = 5000, ifAbsent: () => 5000);
   print(expenses);
 }
