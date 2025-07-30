5550fe1cd4dd4ba611561d94585b0a9d0640bb5c
Murtaza <murtazamandasore@gmail.com>
2025-07-30 17:38:06 +0500

    question 3 done

---
+++ question3.dart
@@ -0,0 +1,29 @@
+void main() {
+  /*
+  Create a list of Days and remove one by one from the end of list.
+   */
+  List days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+}
